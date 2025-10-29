ade34d10073d30d5bca7b67c1d37df3c0981de59
root <l.isheja@alustudent.com>
2025-10-29 11:53:26 -0700

    debug file

--- ssh/100-puppet_ssh_config.pp
+++ ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
-# Puppet manifest to configure SSH client
+## Puppet manifest to configure system-wide SSH client
 file_line { 'Turn off passwd auth':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path  => '/home/ubuntu/.ssh/config',
+  path  => '/etc/ssh/ssh_config',
   line  => 'IdentityFile ~/.ssh/school',
 }
