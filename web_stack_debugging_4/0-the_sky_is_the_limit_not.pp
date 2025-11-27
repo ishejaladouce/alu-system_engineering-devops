e62d1e285a1e920cd05dec345bc94c738c683d64
root <l.isheja@alustudent.com>
2025-11-27 15:26:23 -0800

    Fix Nginx to handle high traffic - increase ULIMIT from 15 to 4096

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,5 @@
 # Fix Nginx to handle high concurrent traffic by increasing file descriptor limits
 exec { 'fix--for-nginx':
-  command => 'sed -i "s/8080/80/g" /etc/nginx/sites-enabled/default',
+  command => 'sed -i "s/15/4096/g" /etc/default/nginx && service nginx restart',
   path    => ['/usr/local/bin', '/bin', '/usr/bin', '/usr/sbin'],
-}
-
-exec { 'increase-ulimit':
-  command => 'sed -i "s/#ULIMIT=\"-n 4096\"/ULIMIT=\"-n 4096\"/g" /etc/default/nginx',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin', '/usr/sbin'],
-}
-
-exec { 'restart-nginx':
-  command => 'service nginx restart',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin', '/usr/sbin'],
-  require => [Exec['fix--for-nginx'], Exec['increase-ulimit']],
 }
